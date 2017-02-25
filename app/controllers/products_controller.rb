bf5f2e2238c1774a3cc07e6f80b3df00cecadfbe
deadpoolme <sunloverz.kg@mailinator.com>
2017-02-25 15:41:18 +0600

    Improve search feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,8 @@ class ProductsController < ApplicationController
     @products = Product.joins(:company).where('companies.active = ? AND products.active = ? AND products.name LIKE ?', true, true, "%#{search}%")
 
     respond_to do |format|
-      format.js   {}
+      flash.now[:notice] = "Can not find this product!" unless @products.present?
+      format.js {}
     end
   end
 end
